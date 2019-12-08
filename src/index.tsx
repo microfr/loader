e5ef826f018de5e656afa3123a306fd146d6e613
oqx <jay.kariesch@gmail.com>
2019-12-08 10:44:09 -0600

    added uri prop and apollo client instance to loader.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import ScriptInjector from "./ScriptInjector";
-import { DocumentNode } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+import ApolloClient, { DocumentNode } from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import ErrorBoundary from "@microfr/error-boundary";
 
 interface Props {
   token?: string;
   query: DocumentNode;
   namespace: string;
+  /*
+  URI of GraphQL server to be passed into apollo client.
+  */
+  uri: string
 }
 
-const _Loader: React.FunctionComponent<Props> = ({ query, namespace }) => {
+const _Loader: React.FunctionComponent<Props> = ({ query, namespace, uri }) => {
   const [src, setSrc] = useState<string>("");
+  const [client, setClient] = useState<ApolloClient<any> | null>(null);
+
   const { loading } = useQuery(query, {
     onCompleted: ({ asset }) => {
       if (asset) {
@@ -20,10 +26,24 @@ const _Loader: React.FunctionComponent<Props> = ({ query, namespace }) => {
       }
     }
   });
+
+  useEffect(() => {
+    setClient(
+      new ApolloClient({
+        uri,
+      })
+    );
+  });
   return (
     <ErrorBoundary>
       <React.Suspense fallback={<div>loading component</div>}>
-        {!loading && src && <ScriptInjector src={src} namespace={namespace} />}
+        {client && (
+          <ApolloProvider client={client}>
+            {!loading && src && (
+              <ScriptInjector src={src} namespace={namespace} />
+            )}
+          </ApolloProvider>
+        )}
       </React.Suspense>
     </ErrorBoundary>
   );
